0e441ff46590d0979efcf7472f71782bef9e8fb3
Harshitha G N <gnharshitha8@gmail.com>
2025-12-22 16:10:35 +0530

    Initial Repo Structure

---
+++ EComm/src/test/java/com/example/EComm/ECommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EComm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
